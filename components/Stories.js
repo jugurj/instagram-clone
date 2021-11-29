f0144d11cada38257e04f5516e864676d25ac8cb
Juri Gurjev <juri.gurjev@twn.ee>
2021-11-29 15:16:48 +0200

    Stories component and faker Implemented Stories component to fetch faker data and render profiles usernames in the front.

--- components/Stories.js
+++ components/Stories.js
@@ -1,5 +1,27 @@
+import faker from "faker";
+import { useEffect, useState } from "react";
+
+const PROFILES_AMOUNT = 20;
+
 function Stories() {
-  return <div>Stories</div>;
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(PROFILES_AMOUNT)].map((_, index) => ({
+      ...faker.helpers.contextualCard(),
+      id: index,
+    }));
+
+    setSuggestions(suggestions);
+  }, []);
+
+  return (
+    <div>
+      {suggestions.map((profile) => (
+        <span key={profile.id}>{profile.username}</span>
+      ))}
+    </div>
+  );
 }
 
 export default Stories;
