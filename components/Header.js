f8a8af65436da8de133e84467a159900be1ad23e
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-01 15:17:34 +0200

    Header right items conditional rendering Added a conditional rendering clause to show/hide header right items depending on the user session login status.

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import {
   MenuIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 function Header() {
   const { data: session } = useSession();
@@ -46,26 +46,31 @@ function Header() {
           />
         </div>
 
-        {/* Right */}
         <div className="flex items-center justify-end space-x-4">
           <MenuIcon className="h-6 w-6 md:hidden cursor-pointer" />
-
           <HomeIcon className="navBtn" />
-          <div className="relative navBtn bottom-1">
-            <PaperAirplaneIcon className="rotate-45" />
-            <div className="absolute top-0 -right-1 text-xs w-4 h-4 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
-              3
-            </div>
-          </div>
-          <PlusCircleIcon className="navBtn" />
-          <UserGroupIcon className="navBtn" />
-          <HeartIcon className="navBtn" />
 
-          <img
-            src={session?.user?.image}
-            alt="Profile picture"
-            className="h-10 rounded-full cursor-pointer"
-          />
+          {session ? (
+            <>
+              <div className="relative navBtn bottom-1">
+                <PaperAirplaneIcon className="rotate-45" />
+                <div className="absolute top-0 -right-1 text-xs w-4 h-4 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                  3
+                </div>
+              </div>
+              <PlusCircleIcon className="navBtn" />
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
+
+              <img
+                src={session?.user?.image}
+                alt="Profile picture"
+                className="h-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={() => signIn()}>Sign In</button>
+          )}
         </div>
       </div>
     </header>
