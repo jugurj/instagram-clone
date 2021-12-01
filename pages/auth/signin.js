d2fd1980f4d0474d33bec481e0b81f2f32f4fced
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-01 15:42:41 +0200

    Fixed login callback url to the home page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -27,7 +27,11 @@ function SignIn({ providers }) {
             <div key={provider.name}>
               <button
                 className="p-3 bg-blue-500 rounded-lg text-white"
-                onClick={() => SignIntoProvider(provider.id)}
+                onClick={() =>
+                  SignIntoProvider(provider.id, {
+                    callbackUrl: "/",
+                  })
+                }
               >
                 Sign in with {provider.name}
               </button>
