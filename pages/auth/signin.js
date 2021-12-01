23605025707ee4622410aafe30d6a474ab1854a0
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-01 15:36:56 +0200

    Custom sign in page Implemented a structure and styles for the custom sign in page. Also added a button in addition to the header one to let user sign in.

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,40 @@
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Head from "next/head";
+import Header from "../../components/Header";
 
 function SignIn({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Head>
+        <title>Instagram clone | auth</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <div className="flex flex-col items-center justify-center py-2 min-h-screen px-14 text-center">
+        <img
+          className="w-80"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
+          alt="Instagram logo"
+        />
+        <p className="font-xs italic">
+          This is nore a real app, it is build for educational purposes only
+        </p>
+
+        <div className="mt-12">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => SignIntoProvider(provider.id)}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,6 @@ export default function Home() {
 
       <Header />
       <Feed />
-
-      {/* Modal */}
     </div>
   );
 }
