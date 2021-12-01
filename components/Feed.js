4ed78b73f812ba4a9313d39267bf438e3c82b904
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-01 13:01:21 +0200

    MiniProfile component Implemented a MiniProfile component to display some bassic user data and logout button.

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,7 @@
+import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
@@ -8,9 +10,16 @@ function Feed() {
         <Stories />
         <Posts />
       </section>
-      <section></section>
+
+      <section className="hidden xl:inline-grid md:col-span-1">
+        <div className="fixed top-16">
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </section>
     </main>
   );
+  ß;
 }
 
 export default Feed;
