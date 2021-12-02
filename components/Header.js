c405f67c50e09b5ada500f039bda223702b66a44
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-02 13:42:27 +0200

    New post modal Implemented a Modal component which opens on header plus button click. Created some basic modal inner structure.

--- components/Header.js
+++ components/Header.js
@@ -70,7 +70,10 @@ function Header() {
                   3
                 </div>
               </div>
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="navBtn"
+              />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
 
