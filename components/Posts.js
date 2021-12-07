db94673b03ddcb6b81b557e61d6090857a583ec4
Juri Gurjev <juri.gurjev@twn.ee>
2021-12-07 12:15:22 +0200

    Implemenrted logic to fetch and show actual posts from the database

--- components/Posts.js
+++ components/Posts.js
@@ -1,3 +1,6 @@
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
 import Post from "./Post";
 
 const POSTS_DATA = [
@@ -21,16 +24,29 @@ const POSTS_DATA = [
 ];
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div>
-      {POSTS_DATA.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          postImg={post.postImg}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          postImg={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
